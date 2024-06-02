09b5cfa6571499687bffba0c32eb56fa6c6e1ad4
Eltis001 <eltisau@live.hk>
2024-06-02 21:36:05 +0800

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,7 +107,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/Main',
+                to: '/docs/intro',
               },
             ],
           },
